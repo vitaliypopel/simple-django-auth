56b4fc74eee0283636094a79f91f1a8f55e0734c
vitaliypopel <vitaliypopel14@gmail.com>
2024-07-28 21:04:51 +0300

    added crispy forms with bootstrap5

--- build_in_auth/build_in_auth/settings.py
+++ build_in_auth/build_in_auth/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'auth_service.apps.AuthServiceConfig',
 ]
 
@@ -121,3 +123,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login/'
+
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
